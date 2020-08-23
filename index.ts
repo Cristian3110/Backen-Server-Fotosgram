c5aff642fb40868d46245c2c539da09247729ac9
cristian3110 <cristiancarrillo311087@gmail.com>
2020-08-22 20:28:49 -0400

    primer commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+import Server from "./classes/server";
+import userRoutes from "./routes/usuario";
+import mongoose from 'mongoose';
+
+
+const server = new Server();
+
+
+
+// Rutas de mi aplicación
+server.app.use('/user', userRoutes);
+
+// Conectar DB
+mongoose.connect('mongodb://localhost:27017/fotosgram', 
+        {useNewUrlParser:true, useCreateIndex:true}, (err)=>{
+    
+    if (err) throw err;
+
+    console.log('Base de Datos ONLINE');
+
+
+        })
+
+// Lavantar Express
+
+server.start(()=>{
+    console.log(`Servidor corriendo en el puerto ${server.port}`);
+});
