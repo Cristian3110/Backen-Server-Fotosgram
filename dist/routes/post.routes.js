0f11d6de03c19bc1583dc9ab8cdc796af54dc9d7
cristian3110 <cristiancarrillo311087@gmail.com>
2020-08-24 21:13:24 -0400

    servicio para obetner los posts paginados

--- dist/routes/post.routes.js
+++ dist/routes/post.routes.js
@@ -13,6 +13,26 @@ const express_1 = require("express");
 const autenticacion_1 = require("../middlewares/autenticacion");
 const post_model_1 = require("../models/post.model");
 const postRoutes = express_1.Router();
+// Crear Posts paginados
+// En esta petición se puede dejar la verificación del token o dejarlo público como en este caso (quitamos en verifiToken)
+postRoutes.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // obteniendo el param para la pagina ? opcional y los trabajamos como un número
+    let pagina = Number(req.query.pagina) || 1;
+    let skip = pagina - 1;
+    skip = skip * 10; // Lógica para el salto de las páginas y salto de registros
+    const posts = yield post_model_1.Post.find()
+        .sort({ _id: -1 }) // con esto especificamos el modo desendentes de los post o registros
+        .skip(skip)
+        .limit(10) // Limitando a que solo muestre los ultimos 10 post
+        .populate('usuario', '-password')
+        .exec();
+    res.json({
+        ok: true,
+        pagina,
+        posts
+    });
+}));
+// Crear Posts
 postRoutes.post('/', [autenticacion_1.verificaToken], (req, res) => {
     const body = req.body;
     body.usuario = req.usuario._id;
--- routes/post.routes.ts
+++ routes/post.routes.ts
@@ -5,6 +5,31 @@ import { Post } from '../models/post.model';
 
 
     const postRoutes = Router();
+    
+    // Crear Posts paginados
+    // En esta petición se puede dejar la verificación del token o dejarlo público como en este caso (quitamos en verifiToken)
+    postRoutes.get('/', async ( req: any, res: Response)=>{
+
+        // obteniendo el param para la pagina ? opcional y los trabajamos como un número
+        let pagina = Number(req.query.pagina) || 1;
+        let skip = pagina -1;
+        skip = skip * 10; // Lógica para el salto de las páginas y salto de registros
+
+        const posts = await Post.find()
+            .sort({_id:-1}) // con esto especificamos el modo desendentes de los post o registros
+            .skip (skip)
+            .limit(10) // Limitando a que solo muestre los ultimos 10 post
+            .populate('usuario', '-password')
+            .exec();
+                
+        res.json({
+            ok: true,
+            pagina,
+            posts
+        });
+    });
+
+    // Crear Posts
 
     postRoutes.post('/', [verificaToken], (req: any, res: Response)=>{
 
