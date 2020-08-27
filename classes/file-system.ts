08cdf93a2ad6f9dd2088d80792856cce4b595402
cristian3110 <cristiancarrillo311087@gmail.com>
2020-08-26 20:35:50 -0400

    Generando un nombre unico a la imagen

--- classes/file-system.ts
+++ classes/file-system.ts
@@ -2,6 +2,7 @@ import { FileUpload } from '../interfaces/file-upload';
 
 import path from 'path';
 import fs from 'fs';
+import uniqid from 'uniqid';
 
 
 // Definiendo la clase para crear la carpetas necesarias para hacer el fileUpload y obtener el path
@@ -13,15 +14,37 @@ export default class FileSystem{
 
         guardarImagenTemporal(file: FileUpload, userID: string){
 
+            // Crear carpetas
             const path = this.crearCarpetaUsuario(userID) // 
+
+            // Nombre archivo
+           const nombreArchivo = this.generarNombreunico(file.name);
+           console.log(file.name); // para observar el nombre original del archivo (imagen)
+           console.log(nombreArchivo); // nombre proporcionado con el Id unico que se estableció
         };
+
+        private generarNombreunico (nombreOriginal: string){
+            
+            // copyXXXXX.png
+            const nombreArr = nombreOriginal.split('.'); // separados por punto ( . )
+            const extension = nombreArr [nombreArr.length -1];
+            
+            // utilizando el paquete para el Id unico (npm install uniqid)
+            const idUnico = uniqid();
+            
+
+
+            return `${idUnico}.${extension}`;
+
+
+        }
 
         private crearCarpetaUsuario(userID:string){
 
             const pathUser= path.resolve(__dirname,'../uploads/', userID);
             const pathUserTemp= pathUser + '/temp/';
 
-            console.log(pathUser);
+            // console.log(pathUser);
 
             const existe = fs.existsSync(pathUser);
 
--- dist/classes/file-system.js
+++ dist/classes/file-system.js
@@ -5,18 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
+const uniqid_1 = __importDefault(require("uniqid"));
 // Definiendo la clase para crear la carpetas necesarias para hacer el fileUpload y obtener el path
 class FileSystem {
     constructor() { }
     ;
     guardarImagenTemporal(file, userID) {
+        // Crear carpetas
         const path = this.crearCarpetaUsuario(userID); // 
+        // Nombre archivo
+        const nombreArchivo = this.generarNombreunico(file.name);
+        console.log(file.name); // para observar el nombre original del archivo (imagen)
+        console.log(nombreArchivo); // nombre proporcionado con el Id unico que se estableció
     }
     ;
+    generarNombreunico(nombreOriginal) {
+        // copyXXXXX.png
+        const nombreArr = nombreOriginal.split('.'); // separados por punto ( . )
+        const extension = nombreArr[nombreArr.length - 1];
+        // utilizando el paquete para el Id unico (npm install uniqid)
+        const idUnico = uniqid_1.default();
+        return `${idUnico}.${extension}`;
+    }
     crearCarpetaUsuario(userID) {
         const pathUser = path_1.default.resolve(__dirname, '../uploads/', userID);
         const pathUserTemp = pathUser + '/temp/';
-        console.log(pathUser);
+        // console.log(pathUser);
         const existe = fs_1.default.existsSync(pathUser);
         if (!existe) {
             fs_1.default.mkdirSync(pathUser);
